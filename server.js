f07fd68a1f95869e78a8e18f42c00a90a7c41a4c
Baimon664 <baimonperasit@gmail.com>
2022-02-10 11:51:56 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const hospital = require("./routes/hospital");
 
 dotenv.config({path:"./config/config.env"});
 
+//connect mongodb
+connectDB();
+
 const app = express();
+
+// add body parser
+app.use(express.json());
 
 app.use("/api/v1/hospitals",hospital);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in', process.env.NODE_ENV,"mode on port",PORT));
+const server = app.listen(PORT, console.log("Server running in ",process.env.NODE_ENV, 'mode on port ', PORT));
+
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err}`);
+    //Close Server & exit process
+    server.close(() => process.exit(1));
+});
